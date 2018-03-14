b92310fb9a11bbe8b0530292807049293e4f5074
xiaorongkui <dovexiaoen@163.com>
2018-03-14 18:04:45 +0800

    肖荣奎，项目开发环境准备

--- app/src/main/java/com/qmkj/jydp/ui/activity/MainActivity.java
+++ app/src/main/java/com/qmkj/jydp/ui/activity/MainActivity.java
@@ -1,9 +1,20 @@
 package com.qmkj.jydp.ui.activity;
 
 import com.qmkj.jydp.R;
+import com.qmkj.jydp.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
 
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initTitle() {
+
+    }
 
     @Override
     protected int contentViewRes() {
@@ -14,4 +25,10 @@ public class MainActivity extends BaseActivity {
     protected void initView() {
 
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
 }
