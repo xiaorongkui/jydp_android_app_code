02f5c0e84dc0f278ff12b2a6797a3a530f4bff38
Yun <614482989@qq.com>
2018-05-28 11:17:33 +0800

    链提取增加输入限制

--- app/src/main/java/com/qmkj/jydp/module/home/presenter/BannerImageLoader.java
+++ app/src/main/java/com/qmkj/jydp/module/home/presenter/BannerImageLoader.java
@@ -3,6 +3,7 @@ package com.qmkj.jydp.module.home.presenter;
 import android.content.Context;
 import android.widget.ImageView;
 
+import com.qmkj.jydp.R;
 import com.qmkj.jydp.base.GlideApp;
 import com.qmkj.jydp.bean.response.HomeDataRes;
 import com.youth.banner.loader.ImageLoader;
@@ -15,8 +16,11 @@ public class BannerImageLoader extends ImageLoader {
     @Override
     public void displayImage(Context context, Object obj, ImageView imageView) {
         if (obj instanceof HomeDataRes.SystemAdsHomepagesListBean) {
-            GlideApp.with(context).load(((HomeDataRes.SystemAdsHomepagesListBean) obj).getAdsImageUrlFormat()).into
-                    (imageView);
+            GlideApp.with(context)
+                    .load(((HomeDataRes.SystemAdsHomepagesListBean) obj).getAdsImageUrlFormat())
+                    .placeholder(R.mipmap.ic_banner_loading)
+                    .error(R.mipmap.ic_banner_fail)
+                    .into(imageView);
         }
     }
 
